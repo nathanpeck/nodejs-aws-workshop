a0f2aca62a20a4eda9bb924088fda625c2843669
Nathan Peck <peckn@amazon.com>
2017-10-06 11:44:31 -0700

    Making the workshop sample code DB more fun

--- 2 - Elastic Beanstalk/code/server.js
+++ 2 - Elastic Beanstalk/code/server.js
@@ -10,32 +10,42 @@ app.use(function *(next){
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
-router.get('/api/users', function *(next) {
-  this.body = db.users;
+router.get('/api/characters', function *(next) {
+  this.body = db.characters;
 });
 
-router.get('/api/users/:userId', function *(next) {
-  const id = parseInt(this.params.userId);
-  this.body = db.users.find((user) => user.id == id);
+router.get('/api/characters/:id', function *(next) {
+  const id = parseInt(this.params.id);
+  this.body = db.characters.find((character) => character.id == id);
 });
 
-router.get('/api/threads', function *() {
-  this.body = db.threads;
+router.get('/api/locations', function *() {
+  this.body = db.locations;
 });
 
-router.get('/api/threads/:threadId', function *() {
-  const id = parseInt(this.params.threadId);
-  this.body = db.threads.find((thread) => thread.id == id);
+router.get('/api/locations/:id', function *() {
+  const id = parseInt(this.params.id);
+  this.body = db.locations.find((location) => location.id == id);
 });
 
-router.get('/api/posts/in-thread/:threadId', function *() {
-  const id = parseInt(this.params.threadId);
-  this.body = db.posts.filter((post) => post.thread == id);
+router.get('/api/characters/by-location/:locationId', function *() {
+  const id = parseInt(this.params.locationId);
+  this.body = db.characters.filter((character) => character.location == id);
 });
 
-router.get('/api/posts/by-user/:userId', function *() {
-  const id = parseInt(this.params.userId);
-  this.body = db.posts.filter((post) => post.user == id);
+router.get('/api/characters/by-gender/:gender', function *() {
+  const gender = this.params.gender.toLowerCase();
+  this.body = db.characters.filter((character) => character.gender == gender);
+});
+
+router.get('/api/characters/by-species/:species', function *() {
+  const species = this.params.species.toLowerCase();
+  this.body = db.characters.filter((character) => character.species.includes(species));
+});
+
+router.get('/api/characters/by-occupation/:occupation', function *() {
+  const occupation = this.params.occupation.toLowerCase();
+  this.body = db.characters.filter((character) => character.occupations.includes(occupation));
 });
 
 router.get('/api/', function *() {
--- 3 - Serverless Lambda/README.md
+++ 3 - Serverless Lambda/README.md
@@ -7,5 +7,9 @@ npm install -g serverless
 2. Configure your serverless environment by logging in to your AWS account:
 
 ```
-serverless login
+serverless config credentials --provider aws --key <access key> --secret <secret key>
 ```
+
+The access key and secret key are the same access key and secret key that was used in the previous Elastic Beanstalk tutorial.
+
+
